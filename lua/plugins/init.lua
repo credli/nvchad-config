cc6369ed43dcca4bc4e7fbc8b170a5160db608e6
Nicholas Credli <nicholas.credli@gmail.com>
2026-01-10 00:23:35 +0200

    add more debugger mapping

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,19 +25,6 @@ return {
     ft = "go",
     dependencies = "mfussenegger/nvim-dap",
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-
-      dapui.setup()
-
-      vim.keymap.set('n', '<F5>', dap.continue, { desc = "Debug: Start/Continue" })
-      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
-
-      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
-      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
-      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
-
-
       require("dap-go").setup {
         -- Tell it to use the dlv binary in your Termux path
         delve = {
@@ -46,6 +33,24 @@ return {
           port = "${port}",
         },
       }
+
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = "Debug: Start/Continue" })
+      vim.keymap.set('n', '<F10>', dap.step_over, { desc = "Debug: Step Over" })
+      vim.keymap.set('n', '<F11>', dap.step_into, { desc = "Debug: Step Into" })
+      vim.keymap.set('n', '<F12>', dap.step_into, { desc = "Debug: Step Out" })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = "Toggle Debug UI" })
+
+      dap.listeners.before.attach.dapui_config = function() dapui.open() end
+      dap.listeners.before.launch.dapui_config = function() dapui.open() end
+      dap.listeners.after.event_initialized.dapui_config = function() dapui.open() end
+      dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+      dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
     end,
   },
 
